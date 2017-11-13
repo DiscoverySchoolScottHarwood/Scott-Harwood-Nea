087e38a0bfd8eef949d5b889ce26e616762b3e61
Scott Harwood <scott.harwood.general@gmail.com>
2017-11-13 22:16:55 +0000

    Some major changes
    Starting to actually work on this project, it may be the one I am least
    proud of on this entire website

--- slider.py
+++ slider.py
@@ -1,6 +1,7 @@
 import sqlite3 as sql
 import tkinter as tk
-import copy
+import time
+import datetime
 USEDFONT = ("Andale Mono", 16)
 #basic code to assemble data set
 NAMES = []
@@ -47,24 +48,63 @@ class slider():
             button.grid(row = self.y+i,column = self.x)
     
     def handleEntry(self,name):
-        print(name)
+        n = name.split()
+        print(n)
+        for x in c.execute("SELECT id,state FROM Students WHERE firstName = ? AND lastName = ?",(n[0],n[1])):
+            if x[1] == "1":
+                c.execute("UPDATE ? SET inTime = ?",(x[0],time.strftime("%H:%M:%S",time.time())))
+            else:
+                record = (datetime.datetime.strftime(datetime.date.today(),"%d-%m-%y"),datetime.datetime.strftime(datetime.datetime.now(),"%H-%M"),"Still logged out")
+                c.execute("INSERT INTO '%s' VALUES (?,?,?)" % x[0],record)
 
+        conn.commit()
     def sliderChanged(self,*args):
         for x in self.textButtons:
             x.grid_forget()
         val = self.slider.get()
         self.genButtonslist(val, val+self.displayed)
+
+    def grid_forget(self):
+        for x in self.textButtons:
+            x.grid_forget()
+        self.slider.grid_forget()
         
 class Main(tk.Frame):
     
     def __init__(self,master):
         super(Main,self).__init__(master)
-        self.grid()
+        self.grid()     
+        self.state = 0
+        data = []
+        for row in c.execute("SELECT firstName,lastName FROM Students WHERE state = 0"):
+            data.append("{} {}".format(row[0],row[1]))
+        self.slider = slider(data,self,1,0,SCREENHEIGHT-200)
         self.createWidgets()
+        self.button_pressed()
+        
 
     def createWidgets(self):
-        self.slider = slider(NAMES,self,0,0,SCREENHEIGHT-50)
-        self.slider2 = slider(NAMES,self,2,0,SCREENHEIGHT-50)
+        self.OutB = tk.Button(self.master,text="Sign Out",font=USEDFONT,width=int(SCREENWIDTH/2),command = self.button_pressed)
+        self.InB = tk.Button(self.master,text="Sign In",font=USEDFONT,width=int(SCREENWIDTH/2),command=self.button_pressed)
+        self.OutB.grid(column=0,row=0)
+        self.InB.grid(column=1,row=0)
+
+    def button_pressed(self):
+        print("button pressed")
+        if self.state == 0:#is in sign out mode
+            print()
+            self.slider.grid_forget()
+            data = []
+            for row in c.execute("SELECT firstName,lastName FROM Students WHERE state = 0"):
+                data.append("{} {}".format(row[0],row[1]))
+            self.slider = slider(data,self,1,0,SCREENHEIGHT-50)
+        else:
+            self.slider.grid_forget()
+            data = []
+            for row in c.execute("SELECT firstName,lastName FROM Student WHERE state = 1"):
+                data.append("{} {}".format(row[0],row[1]))
+            self.slider = slider(data,self,1,0,SCREENHEIGHT-200)
+
 
 root = tk.Tk()
 root.geometry("{}x{}".format(SCREENWIDTH,SCREENHEIGHT))
